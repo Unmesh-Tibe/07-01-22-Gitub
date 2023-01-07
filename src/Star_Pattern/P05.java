361cd1f09e390d53fa97f162c8f1bd5ec7e711b9
com <com@DESKTOP-0NMML1R>
2023-01-07 14:14:15 +0530

    [unmesh]: p05 added

---
+++ src/Star_Pattern/P05.java
@@ -0,0 +1,32 @@
+package Star_Pattern;
+
+public class P05 
+{
+	public static void main(String[] args) 
+	{
+		//*****
+		//****
+		//***
+		//**
+		//*
+		
+		int star = 5;
+		
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=star;j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			star--;
+		}
+       
+		
+		
+		
+		
+	}
+
+}
