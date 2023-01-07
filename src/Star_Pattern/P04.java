bb904170b733fbfea2f567a2be323e3e7dc5e0f4
com <com@DESKTOP-0NMML1R>
2023-01-07 14:07:31 +0530

    [unmesh]: P04 added now

---
+++ src/Star_Pattern/P04.java
@@ -0,0 +1,23 @@
+package Star_Pattern;
+
+public class P04
+{
+	public static void main(String[] args) 
+	{
+		//*
+		//**
+		//***
+		
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+}
